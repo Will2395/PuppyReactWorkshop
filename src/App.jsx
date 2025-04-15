3144bc90b17bf10875aa7fcdec730db56aff55c4
William Kallemeyn <williamkallemeyn@Williams-MacBook-Pro.local>
2025-04-15 16:58:40 -0400

    Updated Puppy list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-
+import { useState } from 'react'
+import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList)
 
+  console.log(puppyList)
   return (
     <div>
-
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
     </div>
   )
 }
