3f38dd7fe86b26d47e20bdeeadc9acfd909b984b
William Kallemeyn <williamkallemeyn@Williams-MacBook-Pro.local>
2025-04-17 01:46:33 -0400

    Updated Css

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,44 @@ import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-
+  const [featPupId, setFeatPupId] = useState(null)
   console.log(puppyList)
+
+  function handleClick() {
+
+  }
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup)
+
   return (
-    <div>
-      {
-        puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
-        })
-      }
+    <div className="App" id="pup-box">
+      <div id="pup-list">
+        <h2>Choose a Puppy:</h2>
+
+        {puppies.map((puppy) => {
+
+          return (
+            <div>
+              <p onClick={() => setFeatPupId(puppy.id)} key={puppy.id}>
+                {puppy.name}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+      <div id="pup-feature">
+        {featPupId && (
+          <div id="feature-box">
+            <div id="divider"></div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
-
 export default App
